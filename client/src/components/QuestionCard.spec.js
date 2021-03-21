a3a2210b9f50730dbff52cf1128c72f0f75fe892
macfrei <lene@7freiheiten.de>
2021-03-21 18:26:19 +0100

    Update tests of QuestionCard component

--- client/src/components/QuestionCard.spec.js
+++ client/src/components/QuestionCard.spec.js
@@ -10,7 +10,7 @@ const cardProps = {
 }
 
 describe('QuestionCard', () => {
-  it('should render a card with a question, an author and information if it has been answered or not', () => {
+  it('should render a card with props', () => {
     const { rerender } = render(<QuestionCard {...cardProps} />)
     expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument()
     expect(screen.getByRole('heading', { level: 2 })).toHaveTextContent(
@@ -20,13 +20,14 @@ describe('QuestionCard', () => {
     expect(screen.getByText('Open question')).toBeInTheDocument()
     rerender(<QuestionCard {...cardProps} isOpenQuestion={false} />)
     expect(screen.getByText('Allready answered')).toBeInTheDocument()
+    expect(screen.getAllByRole('button')).toHaveLength(2)
   })
 
   it('should display "+" if votes are 0 or number of votes', () => {
     const { rerender } = render(<QuestionCard {...cardProps} votes={0} />)
-    expect(screen.getByRole('button')).toHaveTextContent('+')
+    expect(screen.getByRole('button', { name: 'vote' })).toHaveTextContent('+')
     rerender(<QuestionCard {...cardProps} votes={3} />)
-    expect(screen.getByRole('button')).toHaveTextContent('3')
+    expect(screen.getByRole('button', { name: 'vote' })).toHaveTextContent('3')
   })
 
   it('should call function onVote on button click', () => {
@@ -34,11 +35,18 @@ describe('QuestionCard', () => {
     const { rerender } = render(
       <QuestionCard {...cardProps} votes={5} onVote={callback} />
     )
-    userEvent.click(screen.getByRole('button'))
+    userEvent.click(screen.getByRole('button', { name: 'vote' }))
     expect(callback).toHaveBeenCalledTimes(1)
-    expect(screen.getByRole('button')).toHaveTextContent(5)
+    expect(screen.getByRole('button', { name: 'vote' })).toHaveTextContent(5)
     rerender(<QuestionCard {...cardProps} votes={10} onVote={callback} />)
     expect(callback).toHaveBeenCalled()
-    expect(screen.getByRole('button')).toHaveTextContent(10)
+    expect(screen.getByRole('button', { name: 'vote' })).toHaveTextContent(10)
+  })
+
+  it('should call function onDelete on button click', () => {
+    const callback = jest.fn()
+    render(<QuestionCard {...cardProps} onDelete={callback} />)
+    userEvent.click(screen.getByRole('button', { name: 'delete' }))
+    expect(callback).toHaveBeenCalledTimes(1)
   })
 })
